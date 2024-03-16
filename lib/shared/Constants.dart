e5f978e54beace4f1377d9cbb255e5a2feb26fb7
red0074 <pdinesh122003@gmail.com>
2024-03-16 11:42:52 +0530

    flutter app with firebasestorage and hosting

---
+++ lib/shared/Constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String apiKey = 'AIzaSyALzilOfTCWN_GgM_7RCnjMFdyetftOycU';
+  static String appId = '1:5787192523:web:746626b248e99d59699f4e';
+  static String messagingSenderId = '5787192523';
+  static String projectId = 'chatapp-2afaa';
+  final primaryColor = const Color(0xFFee7b64);
+}
