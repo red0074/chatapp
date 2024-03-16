b06937915bb9c89a171cf5645f9945119c81ee12
dinesh_29 <91276996+red0074@users.noreply.github.com>
2024-03-16 11:56:18 +0530

    changing constants for flutter

--- lib/shared/Constants.dart
+++ lib/shared/Constants.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static String apiKey = 'AIzaSyALzilOfTCWN_GgM_7RCnjMFdyetftOycU';
-  static String appId = '1:5787192523:web:746626b248e99d59699f4e';
-  static String messagingSenderId = '5787192523';
-  static String projectId = 'chatapp-2afaa';
+  static String apiKey = 'apikey';
+  static String appId = 'appid';
+  static String messagingSenderId = 'messagingSenderId';
+  static String projectId = 'projectid';
   final primaryColor = const Color(0xFFee7b64);
 }
